aff00bfe3d98eec8899adbf80a6d3303b2826b33
Mouhamed khoutbou thiam <mouhamed.thiam@bbstvnet.com>
2022-03-21 21:04:11 +0000

    stable

--- src/main/scala/com/kh/withgraphx/PlusCourtCheminSansPregel.scala
+++ src/main/scala/com/kh/withgraphx/PlusCourtCheminSansPregel.scala
@@ -1,80 +1,68 @@
-package com.kh.withgraphx
+  package com.kh.withgraphx
 
-import org.apache.log4j.{Level, Logger}
-import org.apache.spark.SparkContext
-import org.apache.spark.rdd.RDD
+  import org.apache.log4j.{Level, Logger}
+  import org.apache.spark.SparkContext
+  import org.apache.spark.rdd.RDD
 
-import scala.collection.mutable.ListBuffer
+  import scala.collection.mutable.ListBuffer
 
-object PlusCourtCheminSansPregel {
+  object PlusCourtCheminSansPregel {
 
-//  def parsLine(line:String)={
-//
-//    val fields = line.split(",")
-//
-//    var adjescenceList = Array[Long]
-//    adjescenceList(0) = fields(0)
-//    var test= (1, (3,4))
-//    var i = 1
-//    while(i < fields.length) {
-//
-//    }
-//
-//    val q1 = fields(0).toLong
-//    val q2 = fields(2).toLong
-//    val alpah = fields(1).toString
-//
-//    Edge(q1,q2,alpah)
-//  }
+    def main(args: Array[String]): Unit =  {
 
-  def main(args: Array[String]): Unit =  {
+      Logger.getLogger("org").setLevel((Level.ERROR))
+      val sc = new SparkContext("local[*]", "PlusCourtChemin_with_pregel")
 
-    Logger.getLogger("org").setLevel((Level.ERROR))
-    val sc = new SparkContext("local[*]", "PlusCourtChemin_with_pregel")
+      var alt = sc.longAccumulator("Alt")
 
-    var graphe:RDD[(Int, (ListBuffer[(Int, Double)], Double))] = null
+      var graphe:RDD[(Int, (ListBuffer[(Int, Double)], Double))] = null
 
-    graphe = sc.parallelize(Seq(
-      (1, (ListBuffer((2, 2.0), (3, 4.0)),0.0)),
-      (2, (ListBuffer((3, 1.0), (5, 7.0)), Double.PositiveInfinity)),
-      (3, (ListBuffer((4, 3.0), (5, 1.0)), Double.PositiveInfinity)),
-      (4, (ListBuffer((6, 1.0)), Double.PositiveInfinity)),
-      (5, (ListBuffer((4, 2.0), (6, 5.0)), Double.PositiveInfinity)),
-      (6, (ListBuffer(), Double.PositiveInfinity))
-    ))
+      graphe = sc.parallelize(Seq(
+        (1, (ListBuffer((2, 2.0), (3, 4.0)),0.0)),
+        (2, (ListBuffer((3, 1.0), (5, 7.0)), Double.PositiveInfinity)),
+        (3, (ListBuffer((4, 3.0), (5, 1.0)), Double.PositiveInfinity)),
+        (4, (ListBuffer((6, 1.0)), Double.PositiveInfinity)),
+        (5, (ListBuffer((4, 2.0), (6, 5.0)), Double.PositiveInfinity)),
+        (6, (ListBuffer(), Double.PositiveInfinity))
+      ))
+      alt.add(1L)
+      while(alt.value > 0){
+        alt.reset()
+        val result1 = graphe.flatMap(value => {
 
-    for(i <- 1 to 10){
+          value._2._1.map(x => (x._1, x._2 + (value._2)._2))
 
-      val result = graphe.flatMap(value => {
-        var list: ListBuffer[(Int, Double)] = ListBuffer[(Int, Double)]()
+        }).reduceByKey((x, y) => math.min(x ,y)).fullOuterJoin(graphe)
 
-        for (x <- (value._2)._1){
+          result1.foreach{
+            case (id, (left, right)) => {
+              val previousValue = right.get._2
+              var value = 0.0
+              if(left == None)  value = previousValue
+              else  value = left.get
+              if(value < previousValue) alt.add(1L)
+            }
+          }
+          val result2 = result1.map{ case (id, (left, right)) => {
+            val previousValue = right.get._2
+            var value = 0.0
+            if(left == None)  value = previousValue
+            else  value = left.get
+            (id, (right.get._1, value))
 
-          list.append((x._1, x._2 + (value._2)._2))
-        }
+          } }
+        graphe = result2
+      }
 
-        list
-      }).reduceByKey((x, y) => math.min(x ,y)).fullOuterJoin(graphe)
-        .map{ case (id, (left, right)) => {
-          var value = 0.0
-          if(left == None)  value = right.get._2
-          else  value = left.get
+      val shortestPaths = graphe.map{
+        case (id, (_, value)) => (id, value)
+      }
 
-          (id, (right.get._1, value))
+      shortestPaths.foreach(println)
 
-        } }
-      graphe = result
+
     }
 
-    val result = graphe.map{
-      case (id, (_, value)) => (id, value)
-    }
-
-    result.foreach(println)
 
 
   }
-
-
-
-}
