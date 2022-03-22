da0265d11cd50c0378d39bd44edad5b07fafde44
Mouhamed khoutbou thiam <mouhamed.thiam@bbstvnet.com>
2022-03-22 09:12:48 +0000

    bug fix

--- src/main/scala/com/kh/withgraphx/PlusCourtCheminSansPregel.scala
+++ src/main/scala/com/kh/withgraphx/PlusCourtCheminSansPregel.scala
@@ -1,38 +1,38 @@
-  package com.kh.withgraphx
+    package com.kh.withgraphx
 
-  import org.apache.log4j.{Level, Logger}
-  import org.apache.spark.SparkContext
-  import org.apache.spark.rdd.RDD
+    import org.apache.log4j.{Level, Logger}
+    import org.apache.spark.SparkContext
+    import org.apache.spark.rdd.RDD
 
-  import scala.collection.mutable.ListBuffer
+    import scala.collection.mutable.ListBuffer
 
-  object PlusCourtCheminSansPregel {
+    object PlusCourtCheminSansPregel {
 
-    def main(args: Array[String]): Unit =  {
+      def main(args: Array[String]): Unit =  {
 
-      Logger.getLogger("org").setLevel((Level.ERROR))
-      val sc = new SparkContext("local[*]", "PlusCourtChemin_with_pregel")
+        Logger.getLogger("org").setLevel((Level.ERROR))
+        val sc = new SparkContext("local[*]", "PlusCourtChemin_with_pregel")
 
-      var alt = sc.longAccumulator("Alt")
+        var alt = sc.longAccumulator("Alt")
 
-      var graphe:RDD[(Int, (ListBuffer[(Int, Double)], Double))] = null
+        var graphe:RDD[(Int, (ListBuffer[(Int, Double)], Double))] = null
 
-      graphe = sc.parallelize(Seq(
-        (1, (ListBuffer((2, 2.0), (3, 4.0)),0.0)),
-        (2, (ListBuffer((3, 1.0), (5, 7.0)), Double.PositiveInfinity)),
-        (3, (ListBuffer((4, 3.0), (5, 1.0)), Double.PositiveInfinity)),
-        (4, (ListBuffer((6, 1.0)), Double.PositiveInfinity)),
-        (5, (ListBuffer((4, 2.0), (6, 5.0)), Double.PositiveInfinity)),
-        (6, (ListBuffer(), Double.PositiveInfinity))
-      ))
-      alt.add(1L)
-      while(alt.value > 0){
-        alt.reset()
-        val result1 = graphe.flatMap(value => {
-
-          value._2._1.map(x => (x._1, x._2 + (value._2)._2))
-
-        }).reduceByKey((x, y) => math.min(x ,y)).fullOuterJoin(graphe)
+        graphe = sc.parallelize(Seq(
+          (1, (ListBuffer((2, 2.0), (3, 4.0)),Double.PositiveInfinity)),
+          (2, (ListBuffer((3, 1.0), (5, 7.0)), Double.PositiveInfinity)),
+          (3, (ListBuffer((4, 3.0), (5, 1.0)), Double.PositiveInfinity)),
+          (4, (ListBuffer((6, 1.0)), Double.PositiveInfinity)),
+          (5, (ListBuffer((4, 2.0), (6, 5.0)), 0.0)),
+          (6, (ListBuffer(), Double.PositiveInfinity))
+        ))
+        alt.add(1L)
+        while(alt.value > 0){
+          alt.reset()
+          val result1 = graphe.flatMap(
+            value => {
+              value._2._1.map(x => (x._1, x._2 + (value._2)._2))
+            }
+          ).reduceByKey( (x, y) => math.min(x ,y) ).fullOuterJoin(graphe)
 
           result1.foreach{
             case (id, (left, right)) => {
@@ -47,22 +47,25 @@
             val previousValue = right.get._2
             var value = 0.0
             if(left == None)  value = previousValue
-            else  value = left.get
+            else {
+              if(previousValue < left.get) value = previousValue
+              else value = left.get
+            }
             (id, (right.get._1, value))
 
           } }
-        graphe = result2
+          graphe = result2
+        }
+
+        val shortestPaths = graphe.map{
+          case (id, (_, value)) => (id, value)
+        }
+
+        shortestPaths.foreach(println)
+
+
       }
 
-      val shortestPaths = graphe.map{
-        case (id, (_, value)) => (id, value)
-      }
-
-      shortestPaths.foreach(println)
 
 
     }
-
-
-
-  }
