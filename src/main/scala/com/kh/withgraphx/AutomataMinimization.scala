e76d5e08883954c3c8fe373ac953a554862c5926
Mouhamed khoutbou thiam <mouhamed.thiam@bbstvnet.com>
2022-03-28 16:32:11 +0000

    stable

--- src/main/scala/com/kh/withgraphx/AutomataMinimization.scala
+++ src/main/scala/com/kh/withgraphx/AutomataMinimization.scala
@@ -1,10 +1,10 @@
-package com.kh
+package com.kh.withgraphx
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkContext
-import org.apache.spark.graphx.{Edge, EdgeDirection, EdgeTriplet, Graph, VertexId}
+import org.apache.spark.graphx._
 
-object AutMinimization2 {
+object AutomataMinimization {
   def parseEdgeLine(line: String) = {
 
     val fields: Array[String] = line.split(",")
@@ -18,29 +18,29 @@ object AutMinimization2 {
 
   def vprog(vertexId: VertexId, vertexData: String, message: String): String = {
 
-    if(message.contains("stop")) {
+    if (message.contains("stop")) {
       val nodeClass = ":".r.split(vertexData)(2)
       println(s"my ID = ${vertexId} I received stop : ${nodeClass}")
       return nodeClass
     }
 
-    if(vertexId == -1) {
+    if (vertexId == -1) {
 
-      if(!message.isEmpty && !message.contains("1") && !vertexData.equals("alt")) return "alt"
+      if (!message.isEmpty && !message.contains("1") && !vertexData.equals("alt")) return "alt"
       return ""
     }
     else {
-      if(message.isEmpty) return vertexData
-      val arrayMessages = "_".r.split(message).sortWith(_.compareTo(_) < 0)
-        .map(msg=> "-".r.split(msg).last)
+      if (message.isEmpty) return vertexData
+      val arrayMessages = message.split("_").sortWith(_.compareTo(_) < 0)
+        .map(msg => msg.split("-").last)
       val currentSize: Int = arrayMessages.toSet.size
-      val previousDatas = ":".r.split(vertexData)
+      val previousDatas = vertexData.split(":")
       val previousSize: Int = previousDatas.head.toInt
       val previousClass: String = previousDatas.last
       var status = ""
-      if(currentSize == previousSize ) status = "0"
-      else  status = "1"
-      val currentData: String = currentSize + ":" + status + ":" + previousClass + arrayMessages.reduceLeft(_+_)
+      if (currentSize == previousSize) status = "0"
+      else status = "1"
+      val currentData: String = currentSize + ":" + status + ":" + previousClass + arrayMessages.reduceLeft(_ + _)
       return currentData
 
     }
@@ -48,20 +48,20 @@ object AutMinimization2 {
 
   def sendMsg(triplet: EdgeTriplet[String, String]): Iterator[(VertexId, String)] = {
 
-    if(triplet.dstAttr.length == 1) return Iterator((triplet.srcId, triplet.attr + "-" + triplet.dstAttr))
+    if (triplet.dstAttr.length == 1) return Iterator((triplet.srcId, triplet.attr + "-" + triplet.dstAttr))
     //    if(triplet.dstAttr.equals("stop")) return Iterator()
 
-    if(triplet.dstId == -1) { // Du Superviseur (noeud special) vers les autres noeuds
+    if (triplet.dstId == -1) { // Du Superviseur (noeud special) vers les autres noeuds
 
-      if(triplet.dstAttr.equals("alt")) return Iterator((triplet.srcId, "stop"))
+      if (triplet.dstAttr.equals("alt")) return Iterator((triplet.srcId, "stop"))
 
-      return Iterator()
+      return Iterator.empty
 
     }
-    val vertexDatas = ":".r.split(triplet.dstAttr)
-    if(vertexDatas.length == 1) return Iterator()
+    val vertexDatas = triplet.dstAttr.split(":")
+    if (vertexDatas.length == 1) return Iterator.empty
     val message = triplet.attr + "-" + vertexDatas.last
-    if(triplet.srcId == -1){ // Des autres noeud vers le Superviseur ( noeud special )
+    if (triplet.srcId == -1) { // Des autres noeud vers le Superviseur ( noeud special )
       val status = vertexDatas(1)
       println(s"in sendMsg ==> status : ${status}")
       return Iterator((triplet.srcId, status))
@@ -79,7 +79,7 @@ object AutMinimization2 {
 
     Logger.getLogger("org").setLevel(Level.ERROR)
 
-    val sc = new SparkContext("spark://spark:7077", "AutMinimization2")
+    val sc = new SparkContext("spark://spark:7077", "AutomataMinimization")
 
     val edgeLines = sc.textFile("edges.txt")
     val vertices = (sc.textFile("vertices.txt")
@@ -87,9 +87,8 @@ object AutMinimization2 {
     val edges = edgeLines.map(parseEdgeLine)
 
     val graph = Graph(vertices, edges)
-    val gra2: Graph[String,String] = graph.pregel[String]("",100,EdgeDirection.In)(vprog,sendMsg, reduceMsg)
-    gra2.edges.saveAsTextFile("./edgesOut")
-    gra2.vertices.saveAsTextFile("./verOut")
+    val gra2: Graph[String, String] = graph.pregel[String]("", 100, EdgeDirection.In)(vprog, sendMsg, reduceMsg)
+    gra2.vertices.collect.foreach(println)
 
   }
 
