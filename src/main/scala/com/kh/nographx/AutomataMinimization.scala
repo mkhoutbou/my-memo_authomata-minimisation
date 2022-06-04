d3c04c509d1d82dde38011107293fd3d0e74b4b1
Mouhamed khoutbou thiam <mouhamed.thiam@bbstvnet.com>
2022-06-04 14:36:25 +0000

    creating new brache for all examples

--- src/main/scala/com/kh/nographx/AutomataMinimization.scala
+++ src/main/scala/com/kh/nographx/AutomataMinimization.scala
@@ -2,25 +2,28 @@
 
   import org.apache.log4j.{Level, Logger}
   import org.apache.spark.SparkContext
+  import org.apache.spark.rdd.RDD
   import org.sparkproject.jetty.server.RequestLog.Collection
 
   object AutomataMinimization {
 
-    class BlockId(blockId: String, size: Int)
+    case class BlockId(value: String, size: Int)
+
+
     def main(args: Array[String]): Unit = {
 
 """----------------------------------------------------------------------------------------------------------------"""
-""" La Partie Configuration """
-Logger.getLogger("org").setLevel((Level.ERROR))
-val sc = new SparkContext("spark://spark:7077", "AutomataMinimizationPregel")
+      """ La Partie Configuration """
+      Logger.getLogger("org").setLevel((Level.ERROR))
+      val sc = new SparkContext("spark://spark:7077", "AutomataMinimization")
 
 """----------------------------------------------------------------------------------------------------------------"""
       """ Le Programme """
 
       // est une variable partagee qui permet au master de savoir si une nouvelle classe a ete cree par un worker
-      val voteAlt = sc.longAccumulator("Alt")
+      val VoteContinue = sc.longAccumulator("Alt")
 
-      var transitionsRDD = sc.parallelize(Seq(
+      val transitionsRDD: RDD[(Long, String, Long)] = sc.makeRDD(Seq(
         (0, "a", 0),
         (0, "b", 3),
         (3, "a", 3),
@@ -31,49 +34,48 @@ val sc = new SparkContext("spark://spark:7077", "AutomataMinimizationPregel")
         (2, "b", 3)
       ))
 
-      var stateBlockIdsRDD = sc.parallelize(Seq(
-        (0, ("0", 1)),
-        (1, ("0", 1)),
-        (2, ("0", 1)),
-        (3, ("1", 1))
+      var stateBlockIdsRDD: RDD[(Long, BlockId)] = sc.makeRDD(Seq(
+        (0, BlockId("0", 1)),
+        (1, BlockId("0", 1)),
+        (2, BlockId("0", 1)),
+        (3, BlockId("1", 1))
       ))
 
-      var result1 = transitionsRDD.map{ case (src, alpha, dest) => (dest, (alpha, src)) }
+      var result0 = transitionsRDD.map{ case (src, alpha, dest) => (dest, (alpha, src)) }
 
-      voteAlt.add(1L)
-      while (voteAlt.value > 0) {
+      VoteContinue.add(1L)
+      while (VoteContinue.value > 0) {
 
-        voteAlt.reset()
+        VoteContinue.reset()
 
-        // Calcul des classes
-        var result2 = stateBlockIdsRDD
-          .join(result1)
+        var result1 = stateBlockIdsRDD
+          .join(result0)
           .map{case (_, (destBlockId, (alpha, src))) => (src, (alpha, destBlockId))}
           .groupByKey()
           .join(stateBlockIdsRDD)
 
-        // Mis a jour des classes
-        stateBlockIdsRDD = result2
+        // Mis a jour des
+        stateBlockIdsRDD = result1
           .map{ case (stateId, (destBlockIdsCollection, statePreviousBlockId)) =>
 
-            val blockId = statePreviousBlockId._1 + destBlockIdsCollection.toSeq.sortWith{
+            val blockId = statePreviousBlockId.value + destBlockIdsCollection.toSeq.sortWith{
               case ((alpha1,_), (alpha2,_)) => alpha1.compareTo(alpha2) < 0
-            }.map{case (_, destBlockId) => destBlockId._1}.reduceLeft(_+_)
-            var blockIdsSet = destBlockIdsCollection.map{case (_, (blockId, _)) => blockId}.toSet
-            blockIdsSet += statePreviousBlockId._1
+            }.map{case (_, destBlockId) => destBlockId.value}.reduceLeft(_+_)
+            var blockIdsSet = destBlockIdsCollection.map{case (_, blockId) => blockId.value}.toSet
+            blockIdsSet += statePreviousBlockId.value
             val newBlockIdSize = blockIdsSet.size
-            val vertexClass= (blockId, newBlockIdSize)
+            val vertexClass= BlockId(blockId, newBlockIdSize)
 
             (stateId, vertexClass)
           }
 
         // detection de creation de nouvelles classes
-        result2.foreach{case (_, (destBlockIdsCollection,statePreviousBlockId)) =>
-            var blockIdsSet = destBlockIdsCollection.map{case (_, (blockId, _)) => blockId}.toSet
-            blockIdsSet += statePreviousBlockId._1
+        result1.foreach{case (_, (destBlockIdsCollection,statePreviousBlockId)) =>
+            var blockIdsSet = destBlockIdsCollection.map{case (_, blockId) => blockId.value}.toSet
+            blockIdsSet += statePreviousBlockId.value
             val newBlockIdSize = blockIdsSet.size
-            if(statePreviousBlockId._2 < newBlockIdSize ) {
-              voteAlt.add(1L)
+            if(statePreviousBlockId.size < newBlockIdSize ) {
+              VoteContinue.add(1L)
             }
         }
 
--- src/main/scala/com/kh/withgraphx/AutomataMinimizationPregel.scala
+++ src/main/scala/com/kh/withgraphx/AutomataMinimizationPregel.scala
@@ -1,117 +1,154 @@
-package com.kh.withgraphx
+  package com.kh.withgraphx
 
-import org.apache.log4j.{Level, Logger}
-import org.apache.spark.SparkContext
-import org.apache.spark.graphx._
+  import org.apache.log4j.{Level, Logger}
+  import org.apache.spark.SparkContext
+  import org.apache.spark.graphx._
 
-object AutomataMinimizationPregel {
-val alphabetSize:Int = 2
-def parseEdgeLine(line: String) = {
+  object AutomataMinimizationPregel {
 
-  val fields: Array[String] = line.split(",")
-  val q1 = fields(0).toLong
-  val q2 = fields(2).toLong
-  val alpha = fields(1)
-  Edge(q1, q2, alpha)
-
-}
+    sealed trait CustomBoolean
+    case object _True extends CustomBoolean
+    case object _False extends CustomBoolean
+    case object _Null extends CustomBoolean
 
 
-def vProg(vertexId: VertexId, vertexData: String, message: String): String = {
+    case class BlockId(value: String, size: Int)
+    case class StateData(blockId: BlockId = null, vote: CustomBoolean = _Null, stopSignal: CustomBoolean = _Null)
+
+    case class Message( blockIdCollection: List[(String, String)] = List(),
+                        votes: CustomBoolean = _Null,
+                        stopSignal: CustomBoolean = _Null )
+
+    val initialMessage: Message = Message()
+
+    val alphabetSize: Int = 2
+    val supervisorId: Int = -1
 
 
-  if (vertexId == -1) { // si c'est le noeud special
+  def vProg(vertexId: VertexId, stateData: StateData, message: Message): StateData = {
 
-    if (!message.isEmpty && !message.contains("1") && !vertexData.equals("alt")) return "alt"
-    return ""
+    if(vertexId == supervisorId) { // si le sommet est le superviseur
+      if(stateData.stopSignal == _Null) return StateData(stopSignal = _False)
+      else {
+        if (stateData.stopSignal == _True) return StateData(stopSignal = _False)
+        else if (message.votes == _True) {
+          return StateData(stopSignal = _True)
+        }
+        else return stateData
+      }
+    }
+    else { // si le sommet est un etat
+      if(message.stopSignal != _True){
+        if(stateData.vote == _Null) return StateData(stateData.blockId, _False)
+        else {
+          val in_blockIdsCollection = message.blockIdCollection
+            .sortWith{case ((_, alpha1), (_, alpha2)) => alpha1.compareTo(alpha2) < 0}
+            .map{case (blockId, _) => blockId}
+          val in_blockIdsSting = in_blockIdsCollection.reduceLeft(_+_)
+          val new_stateBlockId = stateData.blockId.value + in_blockIdsSting
+
+          val new_blocksSize = (stateData.blockId.value :: in_blockIdsCollection).toSet.size
+          var vote: CustomBoolean = _Null
+          if (stateData.blockId.size != new_blocksSize) vote = _False
+          else vote = _True
+
+          return StateData(BlockId(new_stateBlockId, new_blocksSize), vote)
+
+        }
+      } else return StateData(stateData.blockId, _Null)
+    }
   }
-  else { // si c'est un autre noeud ( autre que le noeud especial )
 
-    // si le message c'est le message d'initialisation
-    if (message.isEmpty) return vertexData
+  def sendMsg(triplet: EdgeTriplet[StateData, String]): Iterator[(VertexId, Message)] = {
 
-    // s'il recoit un signal d'arret du noeud especial
-    if (message.contains("stop")) {
-      var nodeClass = vertexData.split(":")(2)
-      var classLength = nodeClass.length
-      classLength = classLength/(alphabetSize + 1)
-      nodeClass = nodeClass.substring(0, classLength)
-      return nodeClass
+    if(triplet.dstId == supervisorId) { // si le sommet envoyeur est le superviseur
+      if(triplet.dstAttr.stopSignal == _True) {
+        val message = (triplet.srcId, Message(stopSignal = _True))
+        Iterator(message)
+      }
+      else return Iterator.empty
+    }else { // si le sommet envoyeur est un etat
+      if(triplet.dstAttr.vote != _Null){
+        if(triplet.srcId == supervisorId) { // si le destinataire est le superviseur
+          Iterator((supervisorId, Message(votes = triplet.dstAttr.vote)))
+        }
+        else{// si l'envoyeur et le destinataire sont tous des etats
+          val blockId_alpha = List((triplet.dstAttr.blockId.value, triplet.attr))
+          Iterator((triplet.srcId, Message(blockIdCollection = blockId_alpha)))
+        }
+      } else return Iterator.empty
     }
 
-    // calcul de la nouvelle classe
-    val arrayMessages = message.split("_").sortWith(_.compareTo(_) < 0)
-      .map(msg => msg.split("-").last)
-    val currentSize: Int = arrayMessages.toSet.size
-    val previousData = vertexData.split(":")
-    val previousSize: Int = previousData.head.toInt
-    val previousClass: String = previousData.last
-    var status = ""
-    if (currentSize == previousSize) status = "0"
-    else status = "1"
-    val currentData: String = currentSize + ":" + status + ":" + previousClass + arrayMessages.reduceLeft(_ + _)
-    return currentData
-
-  }
-}
-
-def sendMsg(triplet: EdgeTriplet[String, String]): Iterator[(VertexId, String)] = {
-
-  // Du Superviseur (noeud special) vers les autres noeuds
-  if (triplet.dstId == -1) {
-
-    if (triplet.dstAttr.equals("alt")) return Iterator((triplet.srcId, "stop"))
-
-    return Iterator.empty
-
-  }
-  // si c'est le message envoye par les autres noeuds (pas le Superviseur)
-  if (triplet.dstAttr.length == 1) return Iterator((triplet.srcId, triplet.attr + "-" + triplet.dstAttr))
-
-  val vertexData = triplet.dstAttr.split(":")
-  if (vertexData.length == 1) return Iterator.empty
-  val message = triplet.attr + "-" + vertexData.last
-
-  // Des autres noeud vers le Superviseur ( noeud special )
-  if (triplet.srcId == -1) {
-    val status = vertexData(1)
-    println(s"in sendMsg ==> status : ${status}")
-    return Iterator((triplet.srcId, status))
   }
 
-  // Entre les noeuds ( sans le Superviseur )
-  return Iterator((triplet.srcId, message))
+  def mergeMsg(message1: Message, message2: Message): Message = {
+    // si le receveur est le superviseur
+    if(message1.votes != _Null && message1.votes != null) {
+      val votes = if(message1.votes == _False || message2.votes == _False) _False else _True
+      Message(votes = votes)
+    }else { // si le receveur est un etat
+      val stop_signal = if(message1.stopSignal == _True || message2.stopSignal == _True) _True else _False
+      val blockIdCollection = message1.blockIdCollection.union(message2.blockIdCollection)
+      Message(blockIdCollection, null, stop_signal)
+    }
+  }
 
-}
+  def main(args: Array[String]): Unit = {
 
-def reduceMsg(message1: String, message2: String): String = {
-  message1 + "_" + message2
-}
+  """----------------------------------------------------------------------------------------------------------------"""
+              """ La Partie Configuration """
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val sc = new SparkContext("local[*]", "AutomataMinimizationPregel")
 
-def main(args: Array[String]): Unit = {
+  """----------------------------------------------------------------------------------------------------------------"""
+              """ Le Programme """
 
-"""----------------------------------------------------------------------------------------------------------------"""
-            """ La Partie Configuration """
-  Logger.getLogger("org").setLevel(Level.ERROR)
-  val sc = new SparkContext("local[*]", "AutomataMinimizationPregel")
+    val transitionsRDD = sc.makeRDD(Seq(
+      // les transitions
+      (0, "a", 0),
+      (0, "b", 3),
+      (3, "a", 3),
+      (3, "b", 1),
+      (1, "a", 1),
+      (1, "b", 2),
+      (2, "a", 2),
+      (2, "b", 3),
 
-"""----------------------------------------------------------------------------------------------------------------"""
-            """ Le Programme """
+      // les arrete entrants et sortants du superviseur
+      (supervisorId, null, 0),
+      (0, null, supervisorId),
+      (supervisorId, null, 1),
+      (1, null, supervisorId),
+      (supervisorId, null, 2),
+      (2, null, supervisorId),
+      (supervisorId, null, 3),
+      (3, null, supervisorId),
+    ))
 
-  val vertices = sc.textFile("vertices.txt")
-    .map(line => line.split(","))
-    .map(part => (part(0).toLong, part(1)))
+    var stateBlockIdsRDD = sc.makeRDD(Seq(
+      // les etats
+      (0, "0"),
+      (1, "0"),
+      (2, "0"),
+      (3, "1"),
+      //le superviseur
+      (supervisorId, "")
+    ))
 
-  val edges = sc.textFile("edges.txt")
-    .map(parseEdgeLine)
+    val edges = transitionsRDD.map{case (src, alpha, dest) => Edge(src, dest, alpha)}
+    val vertices = stateBlockIdsRDD
+      .map{ case (stateId, blockId) =>
+        var stateData:StateData = if(stateId == supervisorId) StateData() else StateData(BlockId(blockId,1))
+        (stateId.toLong, stateData)
+      }
 
-  val graph = Graph(vertices, edges)
-  val gra2: Graph[String, String] = graph.pregel[String]("", 100, EdgeDirection.In)(vProg, sendMsg, reduceMsg)
+    val graph: Graph[StateData, String] = Graph(vertices, edges)
+    val gra2 = graph.pregel[Message](initialMessage, 10, EdgeDirection.In)(vProg, sendMsg, mergeMsg)
 
-"""----------------------------------------------------------------------------------------------------------------"""
-              """ L'Affichage """
-  gra2.vertices.collect.foreach(println)
+  """----------------------------------------------------------------------------------------------------------------"""
+                """ L'Affichage """
+    gra2.vertices.collect.foreach(println)
 
-}
+  }
 
-}
+  }
